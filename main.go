82280fad6466ecd9565c3f15728ceead18eea788
Your Name <you@example.com>
2025-03-16 12:42:07 +0100

    add snippet view and create handlers to main.go

--- main.go
+++ main.go
@@ -11,11 +11,21 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write(([]byte("Hello from Snippetbox")))
 }
 
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write(([]byte("Display a specific snippet...")))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write(([]byte("Display a form create a new snippet...")))
+}
+
 func main() {
 	// Use the http.NewServeMux() function to initialize a new servemux, then
 	// register the home function as the handler for the "/" URL pattern.
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	mux.HandleFunc("/{$}", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	// Print a log message to say that the server is starting.
 	log.Print("starting server on :4000")
