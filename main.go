6bfd98ef1b0fa3ee57669647c2e16c69b19ebd25
Your Name <you@example.com>
2025-03-21 17:57:28 +0100

    add snippetCreatePost handler to handle POST requests for snippet creation

--- main.go
+++ main.go
@@ -34,24 +34,30 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	w.Write(([]byte("Display a form create a new snippet...")))
 }
 
+// Add a snippetCreatePost handler function.
+func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+
+	// Use the Header().Add() method to add a 'Server: Go' header to the
+	// response header map. The first parameter is the header name, and
+	// the second parameter is the header value.
+	w.Header().Add("Server", "Go")
+	// Use the w.WriteHeader() method to send a 201 status code.
+	// and must be the first write in other case doesn't work
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("Save a new snippet..."))
+
+}
+
 func main() {
-	// Use the http.NewServeMux() function to initialize a new servemux, then
-	// register the home function as the handler for the "/" URL pattern.
 	mux := http.NewServeMux()
-	// Prefix the route patterns with the required HTTP method (for now, we will
-	// restrict all three routes to acting on GET requests).
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
+	// Create the new route, which is restricted to POST requests only.
+	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-	// Print a log message to say that the server is starting.
 	log.Print("starting server on :4000")
 
-	// Use the http.ListenAndServe() function to start a new web server. We pass in
-	// two parameters: the TCP network address to listen on (in this case ":4000")
-	// and the servemux we just created. If http.ListenAndServe() returns an error
-	// we use the log.Fatal() function to log the error message and exit. Note
-	// that any error returned by http.ListenAndServe() is always non-nil.
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
