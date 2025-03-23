60f212b991ca52f62212f67ef90058e1daa19c3f
Toni Defez <toni.defez.ibanez@gmail.com>
2025-03-23 16:25:56 +0100

    add serverError and clientError helper functions for improved error handling

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -18,22 +18,13 @@ func (app *Application) Home(w http.ResponseWriter, r *http.Request) {
 
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		// Because the home handler is now a method against the application
-		// struct it can access its fields, including the structured logger. We'll
-		// use this to create a log entry at Error level containing the error
-		// message, also including the request method and URI as attributes to
-		// assist with debugging.
-		app.logger.Error(err.Error(), "method", r.Method, "uri", r.URL.RequestURI())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		app.serverError(w, r, err)
 		return
 	}
 
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		// And we also need to update the code here to use the structured logger
-		// too.
-		app.logger.Error(err.Error(), "method", r.Method, "uri", r.URL.RequestURI())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		app.serverError(w, r, err)
 	}
 }
 
