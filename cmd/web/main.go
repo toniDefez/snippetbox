61a776e18be0a2cd865c5a994e97c0c0624fffb2
Toni Defez <toni.defez.ibanez@gmail.com>
2025-03-23 16:10:30 +0100

    implement command-line flag for server address and replace log with structured logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
-	"log"
+	"flag"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
 func main() {
+	//go run ./cmd/web -addr=":9999"
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	flag.Parse()
+
+	// Use the slog.New() function to initialize a new structured logger, which
+	// writes to the standard out stream and uses the default settings.
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		Level:     slog.LevelDebug,
+		AddSource: true,
+	}))
+
 	mux := http.NewServeMux()
 
 	// Create a file server which serves files out of the "./ui/static" directory.
@@ -23,8 +37,22 @@ func main() {
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-	log.Print("starting server on :4000")
+	// The value returned from the flag.String() function is a pointer to the flag
+	// value, not the value itself. So in this code, that means the addr variable
+	// is actually a pointer, and we need to dereference it (i.e. prefix it with
+	// the * symbol) before using it. Note that we're using the log.Printf()
+	// function to interpolate the address with the log message.
 
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	// Use the Info() method to log the starting server message at Info severity
+	// (along with the listen address as an attribute).
+	logger.Info("starting server", "addr", *addr)
+
+	// And we pass the dereferenced addr pointer to http.ListenAndServe() too.
+	err := http.ListenAndServe(*addr, mux)
+
+	// And we also use the Error() method to log any error message returned by
+	// http.ListenAndServe() at Error severity (with no additional attributes),
+	// and then call os.Exit(1) to terminate the application with exit code 1.
+	logger.Error(err.Error())
+
 }
