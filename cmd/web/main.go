4abfb40294204d137013f3a8b8790b60db106ecf
Toni Defez <toni.defez.ibanez@gmail.com>
2025-03-23 16:28:59 +0100

    refactor main function and extract routes into separate method for improved organization

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -15,57 +15,20 @@ type Application struct {
 }
 
 func main() {
-	//go run ./cmd/web -addr=":9999"
 	addr := flag.String("addr", ":4000", "HTTP network address")
-
 	flag.Parse()
 
-	// Use the slog.New() function to initialize a new structured logger, which
-	// writes to the standard out stream and uses the default settings.
-	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
-		Level:     slog.LevelDebug,
-		AddSource: true,
-	}))
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
-	// Initialize a new instance of our application struct, containing the
-	// dependencies (for now, just the structured logger).
 	app := &Application{
 		logger: logger,
 	}
 
-	mux := http.NewServeMux()
-
-	// Create a file server which serves files out of the "./ui/static" directory.
-	// Note that the path given to the http.Dir function is relative to the project
-	// directory root.
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	// Use the mux.Handle() function to register the file server as the handler for
-	// all URL paths that start with "/static/". For matching paths, we strip the
-	// "/static" prefix before the request reaches the file server.
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("GET /{$}", app.Home)
-	mux.HandleFunc("GET /snippet/view/{id}", app.SnippetView)
-	mux.HandleFunc("GET /snippet/create", app.SnippetCreate)
-	mux.HandleFunc("POST /snippet/create", app.SnippetCreatePost)
-
-	// The value returned from the flag.String() function is a pointer to the flag
-	// value, not the value itself. So in this code, that means the addr variable
-	// is actually a pointer, and we need to dereference it (i.e. prefix it with
-	// the * symbol) before using it. Note that we're using the log.Printf()
-	// function to interpolate the address with the log message.
-
-	// Use the Info() method to log the starting server message at Info severity
-	// (along with the listen address as an attribute).
 	logger.Info("starting server", "addr", *addr)
 
-	// And we pass the dereferenced addr pointer to http.ListenAndServe() too.
-	err := http.ListenAndServe(*addr, mux)
-
-	// And we also use the Error() method to log any error message returned by
-	// http.ListenAndServe() at Error severity (with no additional attributes),
-	// and then call os.Exit(1) to terminate the application with exit code 1.
+	// Call the new app.routes() method to get the servemux containing our routes,
+	// and pass that to http.ListenAndServe().
+	err := http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
-
+	os.Exit(1)
 }
---
+++ cmd/web/routes.go
@@ -0,0 +1,18 @@
+package main
+
+import "net/http"
+
+// The routes() method returns a servemux containing our application routes.
+func (app *Application) routes() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
+	mux.HandleFunc("GET /{$}", app.Home)
+	mux.HandleFunc("GET /snippet/view/{id}", app.SnippetView)
+	mux.HandleFunc("GET /snippet/create", app.SnippetCreate)
+	mux.HandleFunc("POST /snippet/create", app.SnippetCreatePost)
+
+	return mux
+}
