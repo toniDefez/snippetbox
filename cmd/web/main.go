555a84687167b1f82f8b9df3d6a43e7adf9fca84
Toni Defez <toni.defez.ibanez@gmail.com>
2025-03-28 19:27:41 +0100

    implement snippet creation and database insertion logic

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,13 +8,15 @@ import (
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
+	"snippetbox.tonidefez.net/internal/models"
 )
 
 // Define an application struct to hold the application-wide dependencies for the
 // web application. For now we'll only include the structured logger, but we'll
 // add more to this as the build progresses.
 type Application struct {
-	logger *slog.Logger
+	logger   *slog.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
@@ -39,7 +41,8 @@ func main() {
 	defer db.Close()
 
 	app := &Application{
-		logger: logger,
+		logger:   logger,
+		snippets: &models.SnippetModel{DB: db},
 	}
 
 	logger.Info("starting server", "addr", *addr)
