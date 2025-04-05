22f9a7285eacdb42b854ac81b30593c46a749185
Toni Defez <toni.defez.ibanez@gmail.com>
2025-04-05 16:58:25 +0200

    feat: implement Latest method for SnippetModel and add corresponding tests

--- internal/models/snippets_test.go
+++ internal/models/snippets_test.go
@@ -29,3 +29,55 @@ func TestGetSnippet_WithLoadedData(t *testing.T) {
 		t.Errorf("expected title 'Primer snippet'; got %q", snippet.Title)
 	}
 }
+
+func TestInsertSnippet(t *testing.T) {
+	db, err := helpers.SetupTestDB(t)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = helpers.LoadTestDataFromStruct(t, db, testdata.DBFake1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	model := &SnippetModel{DB: db}
+	idSnippet, err := model.Insert("title1", "content1", 7)
+	if err != nil {
+		t.Fatalf("expected to find snippet: %v", err)
+	}
+
+	snippet, err := model.Get(idSnippet)
+	if err != nil {
+		t.Fatalf("expected to find snippet: %v", err)
+	}
+
+	if snippet.Title != "title1" {
+		t.Errorf("expected title 'title1'; got %q", snippet.Title)
+	}
+
+}
+
+func TestGetLatest(t *testing.T) {
+	db, err := helpers.SetupTestDB(t)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = helpers.LoadTestDataFromStruct(t, db, testdata.DBFake1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	model := &SnippetModel{DB: db}
+	snippet, err := model.Latest()
+	if err != nil {
+		t.Fatalf("expected to find snippet: %v", err)
+	}
+
+	if len(snippet) == 0 {
+		t.Fatalf("expected to find at least one snippet; got %d", len(snippet))
+	}
+}
