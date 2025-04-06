db999de0b4fbed28dc8f1876bdbd7b37d1a1b0e6
Toni Defez <toni.defez.ibanez@gmail.com>
2025-04-06 16:09:51 +0200

    refactor: streamline error handling and response writing in render function

--- cmd/web/helper.go
+++ cmd/web/helper.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 	"runtime/debug"
@@ -32,24 +33,19 @@ func (app *Application) clientError(w http.ResponseWriter, status int) {
 }
 
 func (app *Application) render(w http.ResponseWriter, r *http.Request, status int, page string, data templateData) {
-	// Retrieve the appropriate template set from the cache based on the page
-	// name (like 'home.tmpl'). If no entry exists in the cache with the
-	// provided name, then create a new error and call the serverError() helper
-	// method that we made earlier and return.
 	ts, ok := app.templateCache[page]
 	if !ok {
-		err := fmt.Errorf("the template %s does not exist", page)
+		app.serverError(w, r, fmt.Errorf("template %s does not exist", page))
+		return
+	}
+
+	buf := new(bytes.Buffer)
+	err := ts.ExecuteTemplate(buf, "base", data)
+	if err != nil {
 		app.serverError(w, r, err)
 		return
 	}
 
-	// Write out the provided HTTP status code ('200 OK', '400 Bad Request' etc).
 	w.WriteHeader(status)
-
-	// Execute the template set and write the response body. Again, if there
-	// is any error we call the serverError() helper.
-	err := ts.ExecuteTemplate(w, "base", data)
-	if err != nil {
-		app.serverError(w, r, err)
-	}
+	buf.WriteTo(w)
 }
